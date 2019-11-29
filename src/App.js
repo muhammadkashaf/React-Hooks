421cf29deadc427f85f22c33666f85c5624bdfb1
Muhammad Kashaf <31333918+kashafahmed@users.noreply.github.com>
2019-11-30 02:09:57 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import { setState } from 'expect/build/jestMatchersObject';
 
 function App() {
   const [name, setName] = useState("Ahmed")
